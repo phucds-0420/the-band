064abbd2d038cc4d8b4444c9ccb6b3b3191978a7
PhucPhuc <daosyphuc288@gmail.com>
2023-09-04 10:17:49 +0700

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+var getHeading = document.getElementById('about-content')
+console.log([getHeading])
+
+var getPlace = document.getElementsByClassName('place-heading');
+console.log(getPlace);
+
+var tourSection_p = document.querySelectorAll('#tour .col p');
+console.log(tourSection_p);
+
+var tourSection_p2 = document.querySelector('#tour .row');
+console.log(tourSection_p2.getElementsByTagName('p'));
